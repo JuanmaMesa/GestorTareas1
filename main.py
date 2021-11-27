e139deaa330ac430b70fe1a63f7849628b8000a1
Juanma Sánchez <juanma_sme@hotmail.com>
2021-11-27 16:55:22 +0100

    empieza el proyecto

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-def saludo ():
-    print("hola")
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return "Hello World"
+if __name__ == "__main__":
+    app.run(debug=True)
